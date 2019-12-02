efd48714ee9d5c28cfb2e6070d482830a623bc51
wk <human.ipod7@gmail.com>
2019-12-01 23:44:38 -0800

    bingo integrated into menu

--- games/Bingo/Bingo.java
+++ games/Bingo/Bingo.java
@@ -1,22 +1,35 @@
 // Author: Derrick Chan
+package games.Bingo;
 import javax.swing.*;
 
 public class Bingo
 {
     private static OptionGUI option;
-    
-    public static void main(String[] args)
-    {
-        SwingUtilities.invokeLater(new Runnable() 
-        {
-            public void run() 
-            {
-                option = new OptionGUI();
-                option.createAndShowGUI();
-            }
-        });
+
+    public void startBingo(){
+
+      SwingUtilities.invokeLater(new Runnable()
+      {
+          public void run()
+          {
+
+              option = new OptionGUI();
+              option.createAndShowGUI();
+          }
+      });
     }
+    // public static void main(String[] args)
+    // {
+    //     SwingUtilities.invokeLater(new Runnable()
+    //     {
+    //         public void run()
+    //         {
+    //             option = new OptionGUI();
+    //             option.createAndShowGUI();
+    //         }
+    //     });
+    // }
 }
 
 // 7 lines of code
-// 505 total lines 
+// 505 total lines
