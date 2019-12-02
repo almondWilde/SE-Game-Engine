f4206d0ad3546eb67ee4dc0a05f2e0e653baa121
wk <human.ipod7@gmail.com>
2019-12-01 21:49:52 -0800

    button handler added; games folder

--- EpsilonGameLauncher.java
+++ EpsilonGameLauncher.java
@@ -1,18 +1,41 @@
 /**
 	Project: Epsilon Game Launcher
-
-	This program crewates a window for the Epsilon Game Launcher,
+	This program creates a window for the Epsilon Game Launcher,
 	 the main menu, center gaming area, and top menu bar
-
 	@author Armand Wilson
-	Last Edit: 10/24/19
+	Last Edit: 11/28/19
+	11/27/19
+	11/26/19
+	10/24/19
 **/
+/*
+Dear Future Armand,
+	You can change a Jpanel from another class by assigning it to the output of
+	 a JPanel function from the game class.
 
+	There is also a problem with changing the panel from the ActionListener class.
+	Perhaps you could make classes for each game in the EpsilonGameLauncher class,
+	and call them at the appropriate button presses.
+
+	You should really read those GUI ppt slides, theres some concepts here that
+	you just aren't getting. The Java GUI philosophy doesn't jive like what you've
+	done before. You may even find more efficient ways of completing this
+	walpurginacht.
+
+	Sincerely,
+	Armand of 11/28/19
+
+*/
+//access to games folder classes
+import games.*;
+
+//GUI
 import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
 
+//Images
 import java.io.*;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
@@ -22,56 +45,61 @@ public class EpsilonGameLauncher extends JFrame
 	private JMenuBar menuBar;
 	private JMenu NewGame_menu, Quit_menu, Help_menu;
 	private JButton btnTicTacToe,btnBingo,btnThreeCardPoker,btnBlackJack,btnRockPaperScissors,btnMinsweeper;
-	private JPanel PMainMenu, pSouth, pNorth, pCenter;
+	private JPanel pMainMenu, pNorth;
 
+	public JPanel pCenter;
+	public JFrame mainWindow;
 	public EpsilonGameLauncher() throws IOException
 	{
+		mainWindow = new JFrame();
 		menuBar = new JMenuBar();
 		NewGame_menu = new JMenu("New Game");
 		Quit_menu = new JMenu("Help");
 		Help_menu = new JMenu("Quit");
 
-		//menuBar.add(NewGame_menu);
+		menuBar.add(NewGame_menu);
 		menuBar.add(Quit_menu);
 		menuBar.add(Help_menu);
 
-		PMainMenu = new JPanel();
+		pMainMenu = new JPanel();
 		pCenter = new JPanel();
 		pNorth = new JPanel();
-		pSouth = new JPanel();
+
 
 		//button intialization for each game
 		btnTicTacToe = new JButton("TicTacToe");
 		btnTicTacToe.setToolTipText("Play TicTacTie");
-
 		btnBingo = new JButton("Bingo");
 		btnBingo.setToolTipText("Play Bingo");
-
 		btnThreeCardPoker = new JButton("ThreeCardPoker");
 		btnThreeCardPoker.setToolTipText("Play ThreeCardPoker");
-
 		btnBlackJack = new JButton("BlackJack");
 		btnBlackJack.setToolTipText("Play BlackJack");
-
 		btnRockPaperScissors = new JButton("RockPaperScissors");
 		btnRockPaperScissors.setToolTipText("Play Rock, Paper, Scissors");
-
 		btnMinsweeper = new JButton("Minsweeper");
 		btnMinsweeper.setToolTipText("Play Minsweeper");
 
+		btnBingo.addActionListener(new aButtonHandler());
+		btnTicTacToe.addActionListener(new aButtonHandler());
+		btnBlackJack.addActionListener(new aButtonHandler());
+		btnMinsweeper.addActionListener(new aButtonHandler());
+		btnRockPaperScissors.addActionListener(new aButtonHandler());
+		btnThreeCardPoker.addActionListener(new aButtonHandler());
+
 		//buttons are added to the main menu
-		PMainMenu.add(btnTicTacToe);
-		PMainMenu.add(btnBingo);
-		PMainMenu.add(btnThreeCardPoker);
-		PMainMenu.add(btnBlackJack);
-		PMainMenu.add(btnRockPaperScissors);
-		PMainMenu.add(btnMinsweeper);		
+		pMainMenu.add(btnTicTacToe);
+		pMainMenu.add(btnBingo);
+		pMainMenu.add(btnThreeCardPoker);
+		pMainMenu.add(btnBlackJack);
+		pMainMenu.add(btnRockPaperScissors);
+		pMainMenu.add(btnMinsweeper);
+		
 		//set mainMenu appearance
-		PMainMenu.setLayout(new BoxLayout(PMainMenu,BoxLayout.X_AXIS));
-		PMainMenu.setBorder(BorderFactory.createTitledBorder("Games"));
-		PMainMenu.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
-		PMainMenu.setLayout(new GridLayout(1,6));
-		PMainMenu.setBackground(Color.white);
+		pMainMenu.setBorder(BorderFactory.createTitledBorder("Games"));
+		pMainMenu.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
+		pMainMenu.setLayout(new GridLayout(1,6));
+		pMainMenu.setBackground(Color.white);
 
 		//image background for Center panel
 		BufferedImage image = ImageIO.read(new File("backgroundtest.jpg"));
@@ -85,20 +113,60 @@ public class EpsilonGameLauncher extends JFrame
 		pNorth.add(menuBar);
 
 		//add the panels onto the window
-		this.add(PMainMenu,"South");
-		this.add(pCenter,"Center");
-		this.add(pNorth,"North");
+		mainWindow.add(pMainMenu,"South");
+		mainWindow.add(pCenter,"Center");
+		mainWindow.add(pNorth,"North");
 
 		//window settings
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		this.setSize(screenSize.width / 2, screenSize.height / 2);
-		this.setResizable(true);
-		this.setLocation(40,0);
-		this.setTitle("Epsilon Game Launcher");
-		
+		mainWindow.setSize(screenSize.width / 2, screenSize.height / 2);
+		mainWindow.setResizable(true);
+		mainWindow.setLocation(screenSize.width / 4, screenSize.height / 4);
+		mainWindow.setTitle("Epsilon Game Launcher");
+
 		//display menu
-		this.show();
+		mainWindow.show();
 	}
+
+	public class aButtonHandler implements ActionListener{
+		// private EpsilonGameLauncher.aButtonHandler parentClass = this;
+		// super.getClass();
+
+		//public void getSuper() { System.out.println( this.parentClass); }
+		public void actionPerformed(ActionEvent e) {
+			// in here is where the code goes in response to the click
+			if(e.getSource() == btnBingo) {		// Bingo Button!
+				System.out.println("BINGO");
+			}
+			else if(e.getSource() == btnTicTacToe) {		// TicTacToe Button!
+				System.out.println("TicTacToe");
+				TTT new_TTT = new TTT();
+			}
+			else if(e.getSource() == btnMinsweeper) {		// Bingo Button!
+				System.out.println("Minesweeper");
+			}
+			else if(e.getSource() == btnBlackJack) {		// Bingo Button!
+				System.out.println("BlackJack");
+			}
+			else if(e.getSource() == btnRockPaperScissors) {		// Bingo Button!
+				System.out.println("RPS");
+				// getSuper();
+				try{
+					mainWindow.setVisible(false);
+					RPS newRPS = new RPS(mainWindow, pCenter);
+					//newRPS.initialScreen(mainWindow);
+				}
+				catch(Exception IOException){
+					//failed to play a game
+					System.out.println("RPS Failed to open");
+				}
+			}
+			else if(e.getSource() == btnThreeCardPoker) {		// Bingo Button!
+				System.out.println("Three-Card Poker");
+			}
+
+			}
+		}
 
 	public static void main(String[] args) throws IOException
 	{
@@ -109,7 +177,7 @@ public class EpsilonGameLauncher extends JFrame
 					this will enable a single function call for each game upon button click
 					each game function would be able to update the jpanel with the appropriate GUI
 						init() functions may have to take in a JPanel object as argument
-				Adapt LibGui class Slick2D	
+				Adapt LibGui class Slick2D
 		*/
 	}
 }
---
+++ games/RPS.java
@@ -0,0 +1,328 @@
+package games;
+
+//import EpsilonGameLauncher.*;
+
+//RPS dependencies
+import java.util.Scanner;
+import java.io.*;
+import java.util.Random;
+
+//GUI dependecies
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
+import java.awt.event.*;
+
+//GUI
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
+import java.awt.event.*;
+
+//Images
+import java.io.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+class Player{
+	private String weapon; /* A: Rock B: Paper C: Scissors */
+	private boolean isUser;
+
+	Player(boolean isUser){
+		this.isUser = isUser;
+		this.weapon = "";
+		//
+		// Scanner scan = new Scanner(System.in);
+		// boolean valid = false;
+		//
+		//
+		// //not user
+		// if(!this.isUser)
+		// {
+		// 	System.out.println("cpu flag");
+		// 	Random rand = new Random();
+		// 	int randint = rand.nextInt(3);
+		// 	switch( randint ){
+		// 		case 0:
+		// 			this.weapon = "A";	//Rock
+		// 			break;
+		// 		case 1:
+		// 			this.weapon = "B";	//Paper
+		// 			break;
+		// 		case 2:
+		// 			this.weapon = "C";	//Scissors
+		// 			break;
+		// 	}
+		//
+		// 	System.out.println("cpu weapon" + this.weapon);
+		// }
+		// else{
+		// 	System.out.println("user flag");
+		// 	//is user
+		// 	String user_weapon = "";
+		// 	do{
+		// 		System.out.println("Choose your Weapon!\nA) Rock\nB) Paper\nC) Scissors\n\n");
+		// 		weapon = scan.nextLine();
+		// 		weapon.toUpperCase();
+		//
+		// 		valid = weapon.matches("[A-C,a-c]+");
+		// 		if(!valid){
+		// 			System.out.println("Invalid Weapon Choice! Choose another weapon..!\n\n");
+		// 		}
+		// 	}
+		// 	while(!valid);
+		// 	this.weapon = weapon;
+		// }
+	}
+	public String getWeapon() { return this.weapon; }
+	public void setWeapon(String weapon) {this.weapon = weapon;}
+}
+
+
+class InitialScreen extends JFrame{
+	private JButton btnPlay, btnReturn;
+	private JPanel optionsPanel;
+
+	InitialScreen()
+	{
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		optionsPanel = new JPanel();
+		btnPlay = new JButton("Play");
+		btnPlay.addActionListener(new aButtonHandler());
+		btnReturn = new JButton("Return");
+		btnReturn.addActionListener(new aButtonHandler());
+
+		setSize(300, 100);
+		setResizable(true);
+		setLocation(40,0);
+		setTitle("Play?");
+		setBackground(Color.orange);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		add(btnPlay);
+		add(btnReturn);
+		optionsPanel.add(btnPlay);
+		optionsPanel.add(btnReturn);
+		optionsPanel.setBackground(Color.blue);
+		add(optionsPanel);
+		setLocation(screenSize.width / 5, screenSize.height / 5);
+	}
+
+	public class aButtonHandler implements ActionListener{
+		public void actionPerformed(ActionEvent e)
+		{
+			if(e.getSource() == btnPlay){		// Bingo Button!
+				setVisible(false);
+				RPSPlayFrame playRPS = new RPSPlayFrame();
+			}
+			else if(e.getSource() == btnReturn){
+				setVisible(false);
+			}
+		}
+	}
+}
+
+class RPSPlayFrame extends JFrame{
+	private static Player user, cpu;
+	private ButtonGroup userWeapons;
+	private JRadioButton btnRock, btnPaper, btnScissors;
+	private JPanel pWeaponsOptions, pRPSWest, pRPSEast, pRPSCenter;
+	private BufferedImage userWeaponImage;
+	private JLabel userWeaponLabel;
+
+	public RPSPlayFrame(){
+		user = new Player(true);
+		cpu = new Player(false);
+
+		pRPSWest = new JPanel();
+		pRPSWest.setLayout(new FlowLayout());
+
+		btnRock = new JRadioButton("Rock");
+		btnRock.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e){
+				user.setWeapon("Rock");
+				System.out.println(user.getWeapon());
+				try{
+			 		userWeaponImage = ImageIO.read(new File("rock.jpeg"));
+					userWeaponLabel = new JLabel( new ImageIcon(userWeaponImage));
+					pRPSWest.add(userWeaponLabel);
+				}
+				catch(IOException exception){System.out.println("File \"rock.jpg\" Cannot Be Read!");}
+		}
+	});
+		btnPaper = new JRadioButton("Paper");
+		btnPaper.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e){
+				user.setWeapon("Paper");
+				System.out.println(user.getWeapon());
+				try{
+					userWeaponImage = ImageIO.read(new File("paper.png"));
+					userWeaponLabel = new JLabel( new ImageIcon(userWeaponImage));
+					pRPSWest.add(userWeaponLabel);
+				}
+				catch(IOException exception){System.out.println("File \"paper.jpg\" Cannot Be Read!");}
+		}
+	});
+		btnScissors = new JRadioButton("Scissors");
+		btnScissors.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e){
+				user.setWeapon("Scissors");
+				System.out.println(user.getWeapon());
+				try{
+					userWeaponImage = ImageIO.read(new File("scissors.jpg"));
+					userWeaponLabel = new JLabel( new ImageIcon(userWeaponImage));
+					pRPSWest.add(userWeaponLabel);
+				}
+				catch(IOException exception){	System.out.println("File \"scissors.jpg\" Cannot Be Read!");
+			}
+		}
+	});
+		userWeapons = new ButtonGroup();
+
+		userWeapons.add(btnRock);
+		userWeapons.add(btnPaper);
+		userWeapons.add(btnScissors);
+
+		pWeaponsOptions = new JPanel();
+		pWeaponsOptions.setBorder(BorderFactory.createTitledBorder("Choose Your Weapon!"));
+//		pWeaponsOptions.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
+		pWeaponsOptions.setLayout(new GridLayout(1,3));
+		pWeaponsOptions.add(btnRock);
+		pWeaponsOptions.add(btnPaper);
+		pWeaponsOptions.add(btnScissors);
+
+
+		add(pWeaponsOptions, "South");
+		add(pRPSWest, "West");
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation(screenSize.width / 4, screenSize.height / 4);
+		setSize(screenSize.width / 2, screenSize.height / 2);
+		setResizable(true);
+		setTitle("Rock, Paper, Scissors!");
+
+		setVisible(true);
+
+	}
+}
+
+public class RPS{
+	private boolean cont_bool;
+	private String cont_response;
+	private Scanner cont_scanner;
+
+	private JPanel pInit;
+	private JButton btnPlay, btnMainMenu;
+
+	public RPS(JFrame mainWindow, JPanel pCenter) throws IOException{
+		//Rock, Paper, Scissors game start
+		//init screen
+		// pInit = new JPanel();
+		// pInit.add()
+		// add(pInit,"West");
+		InitialScreen init = new InitialScreen();
+		init.show();
+
+
+		//terminal game play
+		// cont_scanner = new Scanner(System.in);
+		// do{
+		// 	//set up radio buttons group
+		// 	//create a mutator for Player.Weapon
+		// 	//as user clicks a weapon on screen, initailize the value of weapon
+		// 	this.user = new Player(true);
+		// 	this.cpu = new Player(false);
+		//
+		// 	System.out.println( this.battle(user, cpu) );
+		//
+		// 	do{
+		// 		//pop up window
+		// 		System.out.println( "Do you want to play again? [Y/N] ");
+		// 		cont_response = cont_scanner.nextLine();
+		// 		cont_response = cont_response.toUpperCase();
+		//
+		// 		if(cont_response.equals("Y"))
+		// 			cont_bool = true;
+		// 		else if( cont_response.equals("N") )
+		// 			cont_bool = false;
+		//
+		// 	}while(!cont_response.equals("Y") && !cont_response.equals("N") );
+		//
+		// }while(cont_bool);
+	}
+
+
+
+	// public JPanel testPanelChange(JPanel test) throws IOException
+	// {
+	// 	test = new JPanel();
+	// 	//background screen, set panel background
+	// 	BufferedImage image = ImageIO.read(new File("RPS_initScreen.jpg"));
+	// 	JLabel label = new JLabel( new ImageIcon(image));
+	//
+	// 	ButtonGroup weapons = new ButtonGroup();
+	// 	JRadioButton rock_radiobtn = new JRadioButton("Rock");
+	// 	JRadioButton paper_radiobtn = new JRadioButton("paper");
+	// 	JRadioButton scissors_radiobtn = new JRadioButton("scissors");
+	// 	weapons.add(rock_radiobtn);
+	// 	weapons.add(paper_radiobtn);
+	// 	weapons.add(scissors_radiobtn);
+	//
+	// 	ButtonGroup enemyWeapons = new ButtonGroup();
+	// 	JRadioButton rock_radiobtn2 = new JRadioButton("Rock");
+	// 	JRadioButton paper_radiobtn2 = new JRadioButton("paper");
+	// 	JRadioButton scissors_radiobtn2 = new JRadioButton("scissors");
+	// 	enemyWeapons.add(rock_radiobtn);
+	// 	enemyWeapons.add(paper_radiobtn);
+	// 	enemyWeapons.add(scissors_radiobtn);
+	//
+	//
+	// 	test.add(rock_radiobtn);
+	// 	test.add(paper_radiobtn);
+	// 	test.add(scissors_radiobtn);
+	// 	test.add(rock_radiobtn2);
+	// 	test.add(paper_radiobtn2);
+	// 	test.add(scissors_radiobtn2);
+	//
+	// 	return test;
+	// }
+
+	/*
+		A - Rock
+		B - Paper
+		C - Scissors
+	*/
+	public static String battle(Player user, Player cpu){
+		String[] returnMessage = {"DRAW", "USER WINS", "CPU WINS", "ERROR ERROR TRY AGAIN"};
+		String uw = user.getWeapon().toUpperCase(), cw = cpu.getWeapon().toUpperCase();
+
+			if( uw.equals( cw ) )
+				return returnMessage[0];
+			else if( cw.equals("Rock") ){		//cpu has a rock
+				if(uw.equals("Paper"))		//user has paper; user wins
+					return returnMessage[1];
+				else if(uw.equals("Scissors"))		//user has scissors; user loses
+					return returnMessage[2];
+			}
+			else if( cw.equals("Paper") ){		//cpu has paper
+				if(uw.equals("Rock"))		//user has rock; user loses
+					return returnMessage[2];
+				else if(uw.equals("Scissors"))		//user has scissors; user wins
+					return returnMessage[1];
+			}
+			else if( cw.equals("Scissors") ){		//cpu has scissors
+				if(uw.equals("Rock"))		//user has rock; user wins
+					return returnMessage[1];
+				else if(uw.equals("Paper"))		//user has paper; user loses
+					return returnMessage[2];
+			}
+			return returnMessage[3];
+	}
+
+	public static void main(String[] args)throws NullPointerException{
+		// RPS newRPS = new RPS();
+		// newRPS.start();
+	}
+}
