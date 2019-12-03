a747a9e43aa31ae9287560092ec0fd22a5ed7af5
dchan54 <47875250+dchan54@users.noreply.github.com>
2019-12-02 21:19:57 -0800

    Update Bingo.java

--- games/Bingo/Bingo.java
+++ games/Bingo/Bingo.java
@@ -18,18 +18,7 @@ public class Bingo
           }
       });
     }
-    // public static void main(String[] args)
-    // {
-    //     SwingUtilities.invokeLater(new Runnable()
-    //     {
-    //         public void run()
-    //         {
-    //             option = new OptionGUI();
-    //             option.createAndShowGUI();
-    //         }
-    //     });
-    // }
 }
 
 // 7 lines of code
-// 505 total lines
+// 523 total lines
