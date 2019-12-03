491347d794d7ce7efbd7c8de83f1ffdba4e0bd47
rflynn04 <54638453+rflynn04@users.noreply.github.com>
2019-12-02 17:54:45 -0800

    Added blackjack files

---
+++ games/Card.java
@@ -0,0 +1,53 @@
+/*
+ Class: Card
+ Creates card objects
+ @author Ryan Flynn
+ Last Edit: 12/2/19
+ 12/1/19
+ 11/27/19
+ 11/20/19
+*/
+package games;
+import java.awt.*;
+import java.util.*;
+
+public class Card {
+        private int suit;
+	private int rank;
+	private static final String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        private static final String[] ranks = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+	
+	public Card(int suit, int rank){
+		this.suit = suit;
+		this.rank = rank;
+	}
+	public int valueOf(){
+		//returns the value of the Card
+		if (this.rank ==0){
+		return 11;
+		}
+		if (this.rank < 10){
+		return rank + 1;
+		}
+		if(this.rank >=10){
+		return 10;
+		}
+		else{
+		return 0;
+		}
+	}
+	public String toString(){
+		//returns the value of the card "[Rank] of [Suit]"
+	        return ranks[this.rank] + " of " + suits[this.suit];
+	}
+	//check to see cards are being created correctly
+	public static void main(String[] args){
+		Card cardTest1 = new Card(0,0);
+		System.out.println("Card x value " + cardTest1.valueOf() + " and is " + cardTest1.toString());
+		Card cardTest2 = new Card(1,4);
+		System.out.println("Card y value " + cardTest2.valueOf() + " and is " + cardTest2.toString());
+		Card cardTest3 = new Card(2,7);
+		System.out.println("Card z value " + cardTest3.valueOf() + " and is " + cardTest3.toString());
+	}
+}
+//27 lines
---
+++ games/Deck.java
@@ -0,0 +1,68 @@
+/*
+ Class: Deck
+ Creates a standard deck of playing cards
+ @author Ryan Flynn
+ Last Edit: 12/2/19
+ 12/1/19
+ 11/27/19
+ 11/20/19
+*/
+package games;
+import java.awt.*;
+import java.util.*;
+
+public class Deck{
+	private Card[] DeckArray = new Card[52];
+	private int index = 0;
+	//creates a deck
+	public Deck() {
+		int num = 0;
+		for(int i =0; i<4; i++){
+			for(int j=0; j<13; j++){
+			Card a = new Card(i,j);
+			 DeckArray[num] = a;
+			 num++;
+			}
+		}
+	}
+	//shuffles
+	public void shuffle(){
+		index = 0;
+		int counter=0;
+		while (counter<1000){
+		Random rand = new Random();
+		int x= rand.nextInt(52);
+		int y= rand.nextInt(52);
+		Card stored = DeckArray[x];
+		DeckArray[x] = DeckArray[y];
+		DeckArray[y] = stored;
+		counter++;
+	}
+	}
+	public boolean hasNextCard(){
+		return index<52;
+	}
+	public Card nextCard(){
+		if(index<52){
+		index++;
+		return DeckArray[index-1];
+		
+		}
+		else{
+		return null;
+		}
+	}
+	public static void main(String[] args){
+		Deck test = new Deck();
+		System.out.println(test.toString());
+				while(test.hasNextCard()){
+			System.out.println(test.nextCard());
+		}
+		test.shuffle();
+		System.out.println(test.toString());
+		while(test.hasNextCard()){
+			System.out.println(test.nextCard());
+		}
+	}
+}
+//38 lines
