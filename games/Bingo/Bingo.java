881f436bc3be7a4dd0378965b13ddf0f1a5c3170
dchan54 <47875250+dchan54@users.noreply.github.com>
2019-12-03 20:34:34 -0800

    Added javadoc comments

--- games/Bingo/Bingo.java
+++ games/Bingo/Bingo.java
@@ -1,11 +1,20 @@
-// Author: Derrick Chan
 package games.Bingo;
 import javax.swing.*;
+/** Main Driver for Bingo Game.
+ * @author Derrick Chan
+ * @version 1.0
+ */
 
 public class Bingo
 {
+    /**
+     * Creates an OptionGUI object that represents the options menu.
+     */
     private static OptionGUI option;
 
+    /**
+    * Runs the options menu GUI.
+    */
     public void startBingo(){
 
       SwingUtilities.invokeLater(new Runnable()
